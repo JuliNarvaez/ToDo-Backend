4ada943c45730499fb55a93a5ff0e252fd527898
JuliNarvaez <0720jano@gmail.com>
2020-11-14 13:04:02 -0500

    CRUD created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const { runInNewContext } = require('vm');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const port = 3000;
@@ -10,6 +11,9 @@ const client = new MongoClient(url);
 
 var db;
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.use(cors("*"))
 
 app.get('/', function(request, response) {  
@@ -18,6 +22,37 @@ app.get('/', function(request, response) {
         console.log("Found the following records");
         response.json(docs);
       });
+});
+
+app.post('/', function(req, res){
+    const toDos = db.collection("toDos");
+    toDos.insertOne(req.body, function(err, resdb){
+        console.log(`Se insertó ${req.body}`)
+        res.json("Postear");
+    });
+});
+
+app.put('/', function(req, res){
+    let query = {
+        id: req.body.id 
+    }
+    var newvalues = { $set: req.body };
+    db.collection("toDos").updateOne(query, newvalues, function (err, resdb){
+        console.log(`Se cambió ${query} por ${newvalues}`);
+        res.json("Putear");
+    })
+    console.log(req.body);
+});
+
+app.delete('/', function(req, res){
+    let query = {
+        id: req.body.id 
+    }
+    db.collection("toDos").deleteOne(query, function (err, resdb){
+        console.log(`Se Borró ${query}`);
+        res.json("Deletear");
+    })
+    console.log(req.body);
 });
 
 app.listen(port, () => {
