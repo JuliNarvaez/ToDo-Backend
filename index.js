94f79f7bd8fe6d5adb128206f33b09c0988961e2
JuliNarvaez <0720jano@gmail.com>
2020-11-14 10:41:03 -0500

    Connected MongoDB with client

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const port = 3000;
 
+const url = 'mongodb://localhost:27017';
+const dbName = 'todo-backend-db';
+const client = new MongoClient(url);
 
-app.get('/', (req, res) => {
-    res.send("Hello world!")
+var db;
+
+app.get('/', function(request, response) {  
+    const toDos = db.collection("toDos");
+    toDos.find({}).toArray(function(err, docs) {
+        console.log("Found the following records");
+        response.json(docs);
+      });
 });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+
+client.connect(function(err, database) {
+    db = database.db(dbName);
+    console.log("Connected successfully to server");
+  });
